240500b817f0a7b747078944972cdada06e070e9
Huzaifa Inshal <huzaifainshal@gmail.com>
2024-03-22 04:19:06 +0500

    'first-commit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RoomsComponent} from './rooms/rooms.component'
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RoomsComponent,NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'hotelinventoryapp';
+  role = 'Admin';
+  
+  @ViewChild('user',{read:ViewContainerRef}) vcr !: ViewContainerRef;
+
+  // @ViewChild('test',{static:true}) name!: ElementRef;
+  // ngOnInit(){
+  //   this.name.nativeElement.innerText = 'blah blah blah'
+  // }
+  
+  ngAfterViewInit(): void {
+      const componentRef = this.vcr.createComponent(RoomsComponent);
+      // componentRef.instance.numberOfRooms = 50;
+  }
 }
